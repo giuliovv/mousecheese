041072946c462d17eb26b5dd87f61af830627f25
Giulio Vaccari <giulio09@marcov.it>
2020-12-01 14:07:21 +0100

    Speed changes

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -3,21 +3,18 @@
 #include <ArduinoJson.h>
 
 
-
-#define led0 16               //D0
-#define led1 5                //D1
-#define led2 4                //D2
-#define led3 0                //D3
-
 DynamicJsonBuffer jsonBuffer;
 
 const char *ssid      = "chair";
 const char *password  = "password";
 
-int sensorValue0 = 0;        
-int sensorValue1 = 0;        
-int sensorValue2 = 0;        
-int sensorValue3 = 0;        
+int forward = 0;        
+int backward = 0;        
+int left = 0;        
+int right = 0;   
+int stopp = 0;
+int forward_level = 0;        
+int backward_level = 0; 
 String sensor_values;
 
 ESP8266WebServer server(80);
@@ -30,25 +27,33 @@ void handleSentVar() {
     Serial.println(sensor_values);
   }
   JsonObject& root = jsonBuffer.parseObject(sensor_values);
-//  if (!root.success()) {
-//    Serial.println("parseObject() failed");
-//    return;
-//  }
-//  if (root.success())
-//  {
-    sensorValue0          = root["forward"].as<int>();
-    sensorValue1          = root["backward"].as<int>();
-    sensorValue2          = root["left"].as<int>();
-    sensorValue3          = root["right"].as<int>();
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  if (root.success())
+  {
+    forward          = root["forward"].as<int>();
+    backward          = root["backward"].as<int>();
+    left          = root["left"].as<int>();
+    right          = root["right"].as<int>();
+    stopp          = root["stop"].as<int>();
 
-//  }
+  }
 
-  Serial.println(sensorValue0);
-  Serial.println(sensorValue1);
-  Serial.println(sensorValue2);
-  Serial.println(sensorValue3);
+  Serial.println(forward);
+  Serial.println(backward);
+  Serial.println(left);
+  Serial.println(right);
+  Serial.println(stopp);
 
-  // toggle_leds();
+  forward_level = forward_level + (forward-backward);
+  backward_level = backward_level + (backward-forward);
+
+  if (forward_level > 4){ forward_level = 4 };
+  if (forward_level < 0){ forward_level = 0 };
+  if (backward_level > 4){ backward_level = 4 };
+  if (backward_level < 0){ backward_level = 0 };
 
   server.send(200, "text/html", "OK");
 }
@@ -59,21 +64,13 @@ void setup() {
   WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
 
-  // pinMode(led0, OUTPUT);
-  // pinMode(led1, OUTPUT);
-  // pinMode(led2, OUTPUT);
-  // pinMode(led3, OUTPUT);
-
   // connect motor pins
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D0, OUTPUT);
-  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);
-  pinMode(D5, OUTPUT);
-  
-  
-  //toggle_leds();                 //turn off all leds as all the sensor values are zero
+  pinMode(D1, OUTPUT); //IN1
+  pinMode(D2, OUTPUT); //IN2
+  pinMode(D0, OUTPUT); //ENA
+  pinMode(D6, OUTPUT); //IN3
+  pinMode(D7, OUTPUT); //IN4
+  pinMode(D5, OUTPUT); //ENB
   
   server.on("/data/", HTTP_GET, handleSentVar); // when the server receives a request with /data/ in the string then run the handleSentVar function
   server.begin();
@@ -89,25 +86,30 @@ void toggle_motors()
   digitalWrite(D0, HIGH);
   digitalWrite(D5, HIGH);
 
-  if (sensorValue0 == 1)  {
+  if (forward == 1)  {
     digitalWrite(D1, HIGH);
     digitalWrite(D2, LOW);
     digitalWrite(D6, HIGH);
     digitalWrite(D7, LOW);
-  } else if(sensorValue1 == 1){
+  } else if(backward == 1){
     digitalWrite(D1, LOW);
     digitalWrite(D2, HIGH);
     digitalWrite(D6, LOW);
     digitalWrite(D7, HIGH);
-  } else if(sensorValue2 == 1) {
+  } else if(left == 1) {
     digitalWrite(D1, HIGH);
     digitalWrite(D2, LOW);
     digitalWrite(D6, LOW);
     digitalWrite(D7, LOW);
-  } else if(sensorValue3 == 1) {
+  } else if(right == 1) {
     digitalWrite(D1, LOW);
     digitalWrite(D2, LOW);
     digitalWrite(D6, HIGH);
+    digitalWrite(D7, LOW);
+  } else if(stopp == 1) {
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D6, LOW);
     digitalWrite(D7, LOW);
   }
 }
